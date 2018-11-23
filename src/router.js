5f0865ddac53b6483ab9aa9cca3504c266e37928
pj <2697857844@qq.com>
2018-11-23 17:34:48 +0800

    这是第三天提交的代码

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import VueRouter from 'vue-router'
+
+// 导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/tabbar/news/newsList.vue'
+import newsInfo from './components/tabbar/news/newsInfo.vue'
+import photoList from './components/tabbar/photos/photoList.vue'
+import photoInfo from './components/tabbar/photos/photoInfo.vue'
+
+// 3. 创建路由对象
+var router = new VueRouter({
+  routes: [ // 配置路由规则
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/search', component: SearchContainer },
+    {path:'/home/newsList',component:NewsList},
+    {path:'/home/newsInfo/:id',component:newsInfo},
+    {path:'/home/photoList',component:photoList},
+    {path:'/home/photoInfo/:id',component:photoInfo}
+  ],
+  linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
+})
+
+// 把路由对象暴露出去
+export default router
